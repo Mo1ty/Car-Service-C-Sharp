58541605997f9b3f1ee7f63d01a092c13dd1039a
zekelinAlex <247635@vutbr.cz>
2023-04-25 15:47:52 +0200

    update master brach and add gui

---
+++ CarServiceApp/Pages/CustomerAddInfo.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CarServiceApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для CustomerAddInfo.xaml
+    /// </summary>
+    public partial class CustomerAddInfo : Window
+    {
+        public CustomerAddInfo()
+        {
+            InitializeComponent();
+        }
+        private void Close_app(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Collapse_app(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
