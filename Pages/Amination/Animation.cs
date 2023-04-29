8f95cedab2101adb3baaee7e842e109989dc0e8c
zekelinAlex <247635@vutbr.cz>
2023-04-29 16:56:39 +0200

    Add values test

---
+++ Pages/Amination/Animation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows;
+
+namespace CarServiceApp.Pages.Amination
+{
+    public static class Animation
+    {
+        public static void AnimateLabel(Label label)
+        {
+            label.Visibility = Visibility.Visible;
+            DoubleAnimation blinkAnimation = new DoubleAnimation
+            {
+                From = 1.0,
+                To = 0.0,
+                Duration = new TimeSpan(0, 0, 0, 0, 500),
+                AutoReverse = true,
+                RepeatBehavior = new RepeatBehavior(2)
+            };
+
+            label.BeginAnimation(Label.OpacityProperty, blinkAnimation);
+        }
+    }
+}
---
+++ Pages/dealPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using CarServiceApp.EFCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CarServiceApp.Pages
+{
+    /// <summary>
+    /// Interaction logic for dealPage.xaml
+    /// </summary>
+    public partial class dealPage : Page
+    {
+        public dealPage()
+        {
+            InitializeComponent();
+            updatecardadagrid();
+        }
+
+        private void Add_Customer(object sender, RoutedEventArgs e)
+        {
+            CreateDeal createDeal = new CreateDeal();
+            createDeal.ShowDialog();
+        }
+
+        private void Refresh_list(object sender, RoutedEventArgs e)
+        {
+            updatecardadagrid();
+        }
+        public void updatecardadagrid()
+        {
+            using (var context = new CarServiceContext())
+            {
+
+                var cars = context.Cars.ToList();
+                var clients = context.Clients.ToList();
+                var deals = context.Deals.ToList();
+
+                var combinedTable = deals
+                    .Join(
+                        clients,
+                        deal => deal.ClientId,
+                        client => client.Id,
+                        (deal, client) => new { Deal = deal, Client = client }
+                    )
+                    .Join(
+                        cars,
+                        dealClient => dealClient.Deal.CarId,
+                        car => car.Id,
+                        (dealClient, car) => new
+                        {
+                            Car = $"{car.Brand} {car.Model}",
+                            Client= $"{dealClient.Client.Name} {dealClient.Client.Surname}",
+                            DealDateTime = dealClient.Deal.DealDateTime,
+                            PaymentAccount = dealClient.Deal.PaymentAccount,
+                            PaymentCode = dealClient.Deal.PaymentCode
+                        }
+                    ).ToList();
+                DealDataGrid.ItemsSource = combinedTable;
+                
+
+            }
+        }
+    }
+}
